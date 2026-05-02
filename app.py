411f74ff47e3f025df09ab26a52f2a67f63404ab
Rachana-Hegde <rachanahegde05@gmail.com>
2026-05-02 20:51:17 +0530

    deployment ready

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ def home():
 # import routes
 from routes import *
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=5000)
